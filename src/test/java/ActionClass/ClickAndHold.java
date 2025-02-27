334b2072eaa0bfe101c2b54bce7d21865b79e89d
LahariMattepally <laharimattepally.2002@gmail.com>
2025-02-27 10:29:04 +0530

    Update ClickAndHold.java

--- src/test/java/ActionClass/ClickAndHold.java
+++ src/test/java/ActionClass/ClickAndHold.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 class ClickAndHold {
-
+//changes done i git hub
 	public static void main(String[] args) throws InterruptedException {
 		// launch the browser
 		WebDriver driver=new ChromeDriver();
